2ddb57c7500dca076f471d25bdb1a50b109e65b7
Erik Malyak <erik@skyhawkmedia.com>
2015-03-16 23:23:28 -0400

    Initial commit

---
+++ EMPageViewController/EMPageViewController/RootViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RootViewController.swift
+//  EMPageViewController
+//
+//  Created by Erik Malyak on 3/16/15.
+//  Copyright (c) 2015 Erik Malyak. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
