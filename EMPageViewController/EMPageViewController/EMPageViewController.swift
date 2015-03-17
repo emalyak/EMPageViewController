1adc68e83715ee44a8466f2e298063ebd94efd18
Erik Malyak <erik@skyhawkmedia.com>
2015-03-17 17:50:31 -0400

    Added datasource

---
+++ EMPageViewController/EMPageViewController/EMPageViewController.swift
@@ -0,0 +1,129 @@
+/*
+
+  EMPageViewController.swift
+  EMPageViewController
+
+  Created by Erik Malyak on 3/16/15.
+  Copyright (c) 2015 Erik Malyak
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+import UIKit
+
+
+protocol EMPageViewControllerDataSource {
+    func em_pageViewController(pageViewController:EMPageViewController, viewControllerLeftOfViewController viewController:UIViewController) -> UIViewController?
+    func em_pageViewController(pageViewController:EMPageViewController, viewControllerRightOfViewController viewController:UIViewController) -> UIViewController?
+}
+
+protocol EMPageViewControllerDelegate {
+    func em_pageViewController(pageViewController:EMPageViewController, willStartTransitionFrom startingViewController:UIViewController, destinationViewController:UIViewController)
+    func em_pageViewController(pageViewController:EMPageViewController, didFinishTransitionFrom startingViewController:UIViewController, destinationViewController:UIViewController, transitionSuccesful:Bool)
+    func em_pageViewController(pageViewController:EMPageViewController, isTransitioningFrom startingViewController:UIViewController, destinationViewController:UIViewController, progress:Float)
+}
+
+class EMPageViewController: UIViewController {
+    
+    var dataSource:EMPageViewControllerDataSource?
+
+    let scrollView:UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.pagingEnabled = true
+        scrollView.scrollsToTop = false
+        scrollView.autoresizingMask = .FlexibleLeftMargin | .FlexibleRightMargin
+        scrollView.setTranslatesAutoresizingMaskIntoConstraints(true)
+        return scrollView
+    }()
+    
+    
+    private var leftViewController:UIViewController?
+    private var currentViewController:UIViewController?
+    private var rightViewController:UIViewController?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.addSubview(scrollView)
+    }
+    
+    func setCurrentViewController(viewController:UIViewController, animated:Bool, completion:(()->())?) {
+        self.scrollView.addSubview(viewController.view)
+        self.addChildViewController(viewController)
+        viewController.didMoveToParentViewController(self)
+        self.currentViewController = viewController
+        
+        if (dataSource != nil) {
+            
+            let beforeViewController = self.dataSource!.em_pageViewController(self, viewControllerLeftOfViewController: viewController)
+            if (beforeViewController != nil) {
+                self.scrollView.addSubview(beforeViewController!.view)
+                self.addChildViewController(beforeViewController!)
+                beforeViewController!.didMoveToParentViewController(self)
+                self.leftViewController = beforeViewController!
+            }
+            
+            let afterViewController = self.dataSource!.em_pageViewController(self, viewControllerRightOfViewController: viewController)
+            if (afterViewController != nil) {
+                self.scrollView.addSubview(afterViewController!.view)
+                self.addChildViewController(afterViewController!)
+                afterViewController!.didMoveToParentViewController(self)
+                self.rightViewController = afterViewController!
+            }
+
+        }
+
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        self.scrollView.frame = self.view.bounds
+        let viewWidth = self.view.bounds.width
+        let viewHeight = self.view.bounds.height
+        
+        var scrollViewContentSizeWidth = viewWidth
+        var scrollViewContentOffset = 0 as CGFloat
+        
+        if (self.leftViewController != nil) {
+            scrollViewContentSizeWidth += viewWidth
+            scrollViewContentOffset += viewWidth
+        }
+        
+        if (self.rightViewController != nil) {
+            scrollViewContentSizeWidth += viewWidth
+        }
+        
+        self.scrollView.contentSize = CGSize(width: scrollViewContentSizeWidth, height: self.view.bounds.height)
+        self.scrollView.contentOffset = CGPoint(x: scrollViewContentOffset, y: 0)
+        
+        self.leftViewController?.view.frame = CGRect(x: 0, y: 0, width: viewWidth, height: viewHeight)
+        self.currentViewController?.view.frame = CGRect(x: scrollViewContentOffset, y: 0, width: viewWidth, height: viewHeight)
+        self.rightViewController?.view.frame = CGRect(x: scrollViewContentOffset + viewWidth, y: 0, width: viewWidth, height: viewHeight)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
--- EMPageViewController/EMPageViewController/RootViewController.swift
+++ EMPageViewController/EMPageViewController/RootViewController.swift
@@ -8,19 +8,44 @@
 
 import UIKit
 
-class RootViewController: UIViewController {
+class RootViewController: UIViewController, EMPageViewControllerDataSource {
 
+    var pageViewController:EMPageViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Instantiate EMPageViewController and set the data source to 'self'
+        let pageViewController = EMPageViewController()
+        pageViewController.dataSource = self
+        
+        // Set the initially selected view controller
+        let currentViewController = self.storyboard!.instantiateViewControllerWithIdentifier("DemoViewController") as UIViewController
+        currentViewController.view.backgroundColor = UIColor(red: 52.0/255.0, green: 73.0/255.0, blue: 94.0/255.0, alpha: 1.0)
+        pageViewController.setCurrentViewController(currentViewController, animated: false, completion: nil)
+        
+        // Add EMPageViewController to the root view controller
+        self.addChildViewController(pageViewController)
+        self.view.addSubview(pageViewController.view)
+        pageViewController.didMoveToParentViewController(self)
+        
+        self.pageViewController = pageViewController
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    // MARK: - EMPageViewController Data Source
+    
+    func em_pageViewController(pageViewController: EMPageViewController, viewControllerLeftOfViewController viewController: UIViewController) -> UIViewController? {
+        let leftViewController = self.storyboard!.instantiateViewControllerWithIdentifier("DemoViewController") as UIViewController
+        leftViewController.view.backgroundColor = UIColor(red: 52.0/255.0, green: 152.0/255.0, blue: 219.0/255.0, alpha: 1.0)
+        return leftViewController
     }
-
+    
+    func em_pageViewController(pageViewController: EMPageViewController, viewControllerRightOfViewController viewController: UIViewController) -> UIViewController? {
+        let rightViewController = self.storyboard!.instantiateViewControllerWithIdentifier("DemoViewController") as UIViewController
+        rightViewController.view.backgroundColor = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
+        return rightViewController
+    }
 
 }
 
