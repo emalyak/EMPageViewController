b9566afad2b64a851901ec770f28c481517c1c75
Erik Malyak <erik@skyhawkmedia.com>
2016-02-05 14:06:18 -0500

    Reorganized repository, created project for CocoaPods

---
+++ EMPageViewController/EMPageViewController.h
@@ -0,0 +1,19 @@
+//
+//  EMPageViewController.h
+//  EMPageViewController
+//
+//  Created by Erik Malyak on 2/5/16.
+//  Copyright © 2016 Erik Malyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EMPageViewController.
+FOUNDATION_EXPORT double EMPageViewControllerVersionNumber;
+
+//! Project version string for EMPageViewController.
+FOUNDATION_EXPORT const unsigned char EMPageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EMPageViewController/PublicHeader.h>
+
+
